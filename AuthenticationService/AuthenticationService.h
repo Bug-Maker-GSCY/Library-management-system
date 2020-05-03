ff51b9737033cde8771c0759f07af17f61194177
yuikony <55555248+yuikony@users.noreply.github.com>
2020-05-03 10:42:24 -0700

    Add files via upload

---
+++ AuthenticationService/AuthenticationService.h
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class AuthenticationService : public User
+{
+private:
+	AuthenticationService AuthenticationService;
+public:
+	void login(account, password);
+	bool signout()
+	void newUser(account, password)
+};
