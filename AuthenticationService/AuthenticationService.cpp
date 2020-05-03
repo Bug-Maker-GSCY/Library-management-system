e7dbf94aa317ca854750f2c80f633b82a92d4efd
yuikony <55555248+yuikony@users.noreply.github.com>
2020-05-03 10:42:12 -0700

    Create AuthenticationService.cpp

---
+++ AuthenticationService/AuthenticationService.cpp
@@ -0,0 +1,29 @@
+#include "Authentication.h"
+
+
+void AuthenticationService::login(account, password)
+{
+	cout << "Enter your account:" << endl;
+   	cin >> account_get;
+  	cout << "Enter your password:" << endl;
+  	cin >> password_get;
+	if account_get = library.account || password_get = library.password;
+	cout << "Login Successful!";
+	else
+	cout << "Login failed, please check account and password."
+  	return 0;
+}
+
+bool AuthenticationService::signout()
+{
+	exit(0);
+}
+
+void
+
+void AuthenticationService::newUser(account, password)
+{
+   string username;
+   string password;
+   cout << "Account created!" << endl;
+}
