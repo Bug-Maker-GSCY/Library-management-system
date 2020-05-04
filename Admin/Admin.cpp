c8e22459fd2fc0fe474af2add844c5f299a2392f
withoutgoinggently <49769237+withoutgoinggently@users.noreply.github.com>
2020-05-03 18:10:38 -0700

    Create Admin.cpp

---
+++ Admin/Admin.cpp
@@ -0,0 +1,9 @@
+#include "Admin.h"
+//Coded by: Molly Morris
+
+void Admin::newUser(string account, string password) {
+//Check if userID = account and Name = password, or if those were intended for different purposes
+}
+bool Admin::removeUserid(string account, string password) {
+
+}
