16fd823e9c743d7f9d6233e8980c922cc086720d
Pengyu Chen <mashirorc>
2020-05-03 23:59:49 -0700

    added database

---
+++ Database/Database.cpp
@@ -0,0 +1,18 @@
+#include "Database.h"
+
+Class Database{
+    void insert(title)
+    {
+
+    }
+
+    Book remove(id)
+    {
+
+    }
+
+    void update(title, author, ISBN, pubDate)
+    {
+
+    }
+}
---
+++ Database/Database.h
@@ -0,0 +1,7 @@
+#include "Database.h"
+Class Database
+{
+    void insert(title);
+    Book remove(id);
+    void update(title, author, ISBN, pubDate);
+}
