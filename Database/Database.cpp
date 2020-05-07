ba21ab3feb1d7e7d8ef8fbd69ff5708ca4cf6072
Pengyu Chen <mashirorc>
2020-05-07 14:34:41 -0700

    psuedo implementation for database

--- Database/Database.cpp
+++ Database/Database.cpp
@@ -1,18 +1,57 @@
 #include "Database.h"
+#include <iostream>
+#include <list>
+using namespace std
 
 Class Database{
-    void insert(title)
-    {
 
+    private list<Book> data;
+    private int lastID;
+
+    public Database()
+    {
+        this->data = {};
+        this->lastID = 0;
     }
 
-    Book remove(id)
+    void insert(string title, string author, int ISBN, string pudDate, string location)
     {
-
+        Book *b = new Book(lastID++ ,title, author, ISBN, pudDate, location);
+        this->data.
     }
 
-    void update(title, author, ISBN, pubDate)
+    bool remove(int id)
     {
+        for(Book b : this->data){
+            if(b->id == id){
+                this->data.remove(b);
+            }
+        }
+        return false;
+    }
 
+    bool update(int id, string title, string author, int ISBN, string pubDate, string location)
+    {
+        for(Book b : this->data){
+            if(b->id == id){
+                b->title = title;
+                b->author = author;
+                b->ISBN = ISBN;
+                b->pubDate = pubDate;
+                b->location = location;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    Book search(string title)
+    {
+        for(Book b : this->data){
+            if(b->title == title){
+                return *b;
+            }
+        }
+        return false;
     }
 }
