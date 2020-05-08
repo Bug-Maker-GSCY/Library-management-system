66164f4ccea2d760b65daab6e23a5eadaa24fccc
withoutgoinggently <49769237+withoutgoinggently@users.noreply.github.com>
2020-05-07 17:53:53 -0700

    Added comments

--- Admin/Admin.h
+++ Admin/Admin.h
@@ -7,8 +7,11 @@ using namespace std;
 class Admin : public User
 {
 private:
+	//Added since we created the diagram in milestone 5
 	List <User> userList;
 public:
+	//Adds a user to the list
 	void newUser(string account, string password);
+	//Removes a user from the list
 	bool removeUserid(string account, string password);
 };
