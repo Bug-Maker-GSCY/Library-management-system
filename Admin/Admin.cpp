3f89a92c61dc44e7c5ca8a01733e392b39086d54
withoutgoinggently <49769237+withoutgoinggently@users.noreply.github.com>
2020-05-07 20:24:31 -0700

    Update Admin.cpp

--- Admin/Admin.cpp
+++ Admin/Admin.cpp
@@ -1,9 +1,20 @@
-#include "Admin.h"
+#pragma once
+#include <iostream>
+#include <string>
+#include "User.h"
 //Coded by: Molly Morris
-
-void Admin::newUser(string account, string password) {
-//Check if userID = account and Name = password, or if those were intended for different purposes
-}
-bool Admin::removeUserid(string account, string password) {
-
-}
+using namespace std;
+class Admin : public User
+{
+private:
+	//Added since we created the diagram in milestone 5
+	vector<User*> userList;
+public:
+	//Initializes an admin, given an account and password
+	Admin(string account, string password);
+	//Adds a user to the list
+	bool newUser(string account, string password);
+	//Removes a user from the list
+	bool removeUserid(string account);
+	bool findUser(string account);
+};
