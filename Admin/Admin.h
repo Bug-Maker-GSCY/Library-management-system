a7c16705accb872a53c7c965802c56cc1d02bcf0
withoutgoinggently <49769237+withoutgoinggently@users.noreply.github.com>
2020-05-07 18:00:03 -0700

    Added a constructor

--- Admin/Admin.h
+++ Admin/Admin.h
@@ -10,6 +10,7 @@ private:
 	//Added since we created the diagram in milestone 5
 	List <User> userList;
 public:
+	Admin(string account, string password);
 	//Adds a user to the list
 	void newUser(string account, string password);
 	//Removes a user from the list
